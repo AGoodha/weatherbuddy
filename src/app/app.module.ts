0bf50e5d4ef981b552daffb7dffede1f52f68ccc
AGoodha <artleyg@gmail.com>
2023-02-25 21:34:40 -0700

    fixed API call for detailed forecast URL

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { LocationService } from './location.service';
 import { WeatherApiService } from './weather-api.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
   providers: [
     LocationService,
     Geolocation,
     WeatherApiService,
+    HttpClientModule,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/weather-api.service.ts
+++ src/app/weather-api.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { LocationService } from './location.service';
-import { map } from 'rxjs/operators';
-import { stringify } from 'querystring';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +9,7 @@ export class WeatherApiService {
   constructor(private http: HttpClient, public locationservice: LocationService) { }
   async location() {
     let location = await this.locationservice.getLocation().then((location) => {
-      //console.log(location + "location on tbts")
+     // console.log(location + "location on tbts")
       return location
     }).catch((error) => {
       console.log('Error parsing returned location', error);
@@ -23,33 +21,31 @@ export class WeatherApiService {
     var timestamp = new Date().getTime();
     return timestamp;
   }
-  
 
-  async firstWeatherCall(){
+
+  async firstWeatherCall() {
+
+    //get location 
     let location = await this.location().then((val) => {
       return val;
     })
 
     const apiURL = "https://api.weather.gov/points/" + location;
-   //TODO add interfaces to define the structure of the response to be more type safe
+    //TODO add interfaces to define the structure of the response to be more type safe
     //get url for detailed forecast
-    let periods: string;
     let firstUrl: any;
-     firstUrl =  this.http.get(apiURL).subscribe((response: any) => {
-          for (let forecast of response) {
-          let  properties = forecast.properties;
-               periods = properties.forecast;
-            // Access other properties as needed
-          }
-          console.log("periods" + periods)
-          return periods;
-        });
-
+    let forecasturl: string;
+    firstUrl = this.http.get(apiURL).subscribe((response: any) => {
+     let properties = response["properties"];
+      forecasturl = properties["forecast"];
+      console.log("forecasturl" + forecasturl);
+      return forecasturl;
+    });
 
 
     const sevendayforecast = new Array;
     //get detailed forecast
-    let result = await this.http.get(firstUrl).subscribe((response: any) =>  { // Success
+    let result = this.http.get(firstUrl).subscribe((response: any) => {
       // console.log("log" + res)
       let properties = response["properties"];
       let periods = properties["periods"];
@@ -57,27 +53,28 @@ export class WeatherApiService {
       for (var i = 0; i < periods.length; i++) {
         let forecast = periods[i];
         let temp = forecast["temperature"];
-        let name = forecast["name"];
-        let starttime = JSON.stringify(forecast["startTime"]);
-        let enddateindex = starttime.lastIndexOf(":") - 12;
-        let date = starttime.substring(0, enddateindex);
-        let endtime = forecast["endTime"];
-        let isdaytime = forecast['isDaytime'];
-        let windspeed = forecast["windSpeed"];
-        let windDirection = forecast["windDirection"];
-        let detailedForecast = forecast["detailedForecast"];
-        var detailedForecastString = JSON.stringify(forecast["detailedForecast"])
-        var percentprecipIndexEnd = detailedForecastString.toUpperCase().indexOf("%") - 1;
-        var percentprecipIndexStart = percentprecipIndexEnd - 3;
-        var percentprecip = (detailedForecast.substring(percentprecipIndexStart, percentprecipIndexEnd))
-        if (percentprecip.length <= 0) {
-          percentprecip = "0";
-        };
-        sevendayforecast.push([date, name, isdaytime, temp, percentprecip]);
+        // let name = forecast["name"];
+        // let starttime = JSON.stringify(forecast["startTime"]);
+        // let enddateindex = starttime.lastIndexOf(":") - 12;
+        // let date = starttime.substring(0, enddateindex);
+        // let endtime = forecast["endTime"];
+        // let isdaytime = forecast['isDaytime'];
+        // let windspeed = forecast["windSpeed"];
+        // let windDirection = forecast["windDirection"];
+        // let detailedForecast = forecast["detailedForecast"];
+        // var detailedForecastString = JSON.stringify(forecast["detailedForecast"])
+        // var percentprecipIndexEnd = detailedForecastString.toUpperCase().indexOf("%") - 1;
+        // var percentprecipIndexStart = percentprecipIndexEnd - 3;
+        // var percentprecip = (detailedForecast.substring(percentprecipIndexStart, percentprecipIndexEnd))
+        // if (percentprecip.length <= 0) {
+        //   percentprecip = "0";
+        // };
+        sevendayforecast.push([temp]);
       }
       return sevendayforecast;
     })
     this.firstWeatherCallTimeStamp();
+    console.log("result1" + sevendayforecast)
     return result;
 
   }
