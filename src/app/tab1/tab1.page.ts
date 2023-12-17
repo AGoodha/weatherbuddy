0990f600b8d6f3f64ac2b5f8550e0323dcd66233
AGoodha <artleyg@gmail.com>
2023-12-17 10:40:53 -0700

    working on forecast display logic

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { WeatherApiService } from '../weather-api.service';
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,23 +8,51 @@ import { WeatherApiService } from '../weather-api.service';
 })
 export class Tab1Page {
   forecastarray: any = [];
+  processedForecastArray: any[]; // Declare the property here
+
 
   constructor(public weatherapiservice: WeatherApiService) {
     this.weatherapiservice.location();
     this.wetherCall();
+     // Example original data
+     let forecastarray = [
+      "TodayTemperature58Precipitation0",
+      "TonightTemperature26Precipitation0",
+      // Add the rest of your forecast data here...
+    ];
+
+    this.processedForecastArray = forecastarray.map(forecastString => {
+      let dayMatch = forecastString.match(/^[A-Za-z ]+/);
+      let tempMatch = forecastString.match(/Temperature(\d+)/);
+      let precipMatch = forecastString.match(/Precipitation(\d+)/);
+
+      return {
+        day: dayMatch ? dayMatch[0].trim() : '',
+        temperature: tempMatch ? parseInt(tempMatch[1]) : 0,
+        precipitation: precipMatch ? parseInt(precipMatch[1]) : 0
+      };
+    });
   }
-  //Get forecast for interpolation with html page
-wetherCall(){
-   //get current time for run limter
-  var timestamp = new Date().getTime();
- //check if enough time has passed to execute again
- var execute = this.weatherapiservice.verifyRunLimiter(timestamp);
-  if(execute) {
-  this.weatherapiservice.firstWeatherCall().then(result=> {
-    this.forecastarray = result
-   console.log("executed weather call from tab1" + this.forecastarray);
-    })
-   }
+  
+
+  // Get forecast for interpolation with html page
+  wetherCall() {
+    // get current time for run limiter
+    var timestamp = new Date().getTime();
+    // check if enough time has passed to execute again
+    var execute = this.weatherapiservice.verifyRunLimiter(timestamp);
+    if (execute) {
+      this.weatherapiservice.firstWeatherCall().then(result => {
+        this.forecastarray = result;
+        console.log("executed weather call from tab1" + this.forecastarray);
+      });
+    }
+  }
+
 }
+
+
+
+
 //TODO finish run limter on wetaher API call and location call
-}
+
