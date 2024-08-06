feb60d0030b112ac9c69b0a2c442e6056bbc5489
AGoodha <artleyg@gmail.com>
2024-08-06 15:33:46 -0600

    add QPF

--- src/app/weather-api.service.ts
+++ src/app/weather-api.service.ts
@@ -1,110 +1,27 @@
-// import { Injectable } from '@angular/core';
-// import { HttpClient } from '@angular/common/http';
-// import { LocationService } from './location.service';
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class WeatherApiService {
-//   constructor(private http: HttpClient, public locationservice: LocationService) { }
-//   async location() {
-//     let location = await this.locationservice.getLocation().then((location) => {
-//      // console.log(location + "location on tbts")
-//       return location
-//     }).catch((error) => {
-//       console.log('Error parsing returned location', error);
-//     });
-//     return location;
-//   }
-//   //get time stamp only used in firstWeatherCall
-//   firstWeatherCallTimeStamp() {
-//     var timestamp = new Date().getTime();
-//     return timestamp;
-//   }
-
-
-//   async firstWeatherCall() {
-
-//     //get location 
-//     let location = await this.location().then((val) => {
-//       return val;
-//     })
-
-//     const apiURL = "https://api.weather.gov/points/" + location;
-//     //TODO add interfaces to define the structure of the response to be more type safe
-//     //get url for detailed forecast
-  
-    
-    
-//     let firstUrl =  await this.http.get(apiURL).toPromise().then((response: any) => {
-//      let properties = response["properties"];
-//      let forecasturl = properties["forecast"];
-//      // console.log("forecasturl" + forecasturl);
-//       return forecasturl;
-//     });
-
-//     console.log("forecasturl1" + firstUrl)
-//     const sevendayforecast = new Array;
-//     //get detailed forecast
-//     let result = await this.http.get(firstUrl).toPromise().then((response: any) => {
-//       // console.log("log" + res)
-//       let properties = response["properties"];
-//       let periods = properties["periods"];
-//       // console.log("log" + properties)
-//       for (var i = 0; i < periods.length; i++) {
-//         let forecast = periods[i];
-//         let temp = forecast["temperature"];
-//          let name = forecast["name"];
-//         let starttime = JSON.stringify(forecast["startTime"]);
-//         let enddateindex = starttime.lastIndexOf(":") - 12;
-//         let date = starttime.substring(0, enddateindex);
-//         let endtime = forecast["endTime"];
-//         let isdaytime = forecast['isDaytime'];
-//         let windspeed = forecast["windSpeed"];
-//         let windDirection = forecast["windDirection"];
-//         let detailedForecast = forecast["detailedForecast"];
-//         var detailedForecastString = JSON.stringify(forecast["detailedForecast"])
-//         var percentprecipIndexEnd = detailedForecastString.toUpperCase().indexOf("%") - 1;
-//         var percentprecipIndexStart = percentprecipIndexEnd - 3;
-//         var percentprecip = (detailedForecast.substring(percentprecipIndexStart, percentprecipIndexEnd))
-//         if (percentprecip.length <= 0) {
-//           percentprecip = "0";
-//         };
-//         sevendayforecast.push([name + "Temperature" +temp + "Precipitation" + percentprecip]);
-//       }
-//       return sevendayforecast;
-//     })
-//     this.firstWeatherCallTimeStamp();
-//     console.log("result1" + sevendayforecast)
-//     return result;
-
-//   }
-//   //Compare given time with timestamp of last API ccall only for first weather API call. 
-// verifyRunLimiter(timestamp: number) {
-//   var execute = false;
-//   var firsttensecoinds= timestamp + 1000000;
-//   if((timestamp <= firsttensecoinds)){
-//     console.log("tensecondprint=" + firsttensecoinds)
-//     var execute = true;
-//   }
-//   return execute;
-// }
-  //TODO forcast will be collected on demand and used once a day then purged
-//}
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { LocationService } from './location.service';
 
-// Interface to define the structure of a weather forecast
-interface Forecast {
+interface ForecastPeriod {
   temperature: number;
   isDaytime: boolean;
-  HDD: number;
-  CDD: number;
-  precipitationProbability: number;
-  estimatedPrecipitation: number;
+  probabilityOfPrecipitation: {
+    value: number;
+  };
   detailedForecast: string;
-  mlModelPrediction?: string; // This will hold the prediction from the ML model
+  startTime: string;
+  endTime: string;
+}
+
+interface GridData {
+  properties: {
+    quantitativePrecipitation: {
+      values: Array<{
+        value: number;
+        validTime: string;
+      }>
+    }
+  }
 }
 
 @Injectable({
@@ -128,41 +45,42 @@ export class WeatherApiService {
   async firstWeatherCall() {
     const location = await this.location();
     if (!location) return;
-  
-    const apiURL = `https://api.weather.gov/points/${location}`;
-    const forecastUrl = await this.http.get(apiURL).toPromise().then((response: any) => response?.properties?.forecast);
-    
-    const forecastAPI = `${forecastUrl}?units=us`
-    const forecastData = await this.http.get(forecastAPI).toPromise();
-    
-    console.log("Forecast URL: " + forecastUrl);
-    if (!forecastUrl) return;
-  
-    const result = await this.http.get(forecastAPI).toPromise().then((response: any) => {
-      const periods: Forecast[] = response?.properties?.periods.map((forecast: any) => {
-        const { temperature, isDaytime, probabilityOfPrecipitation} = forecast; 
-        const precipitationProbability = probabilityOfPrecipitation?.value ?? 0;
-  
-        return {
-          temperature,
-          isDaytime,
-          precipitationProbability,
-          detailedForecast: forecast.detailedForecast
-        };
-      });
-      return periods;
-    });
-  
-    console.log("Weather Data:", result);
-    return result;
-  }
-  
 
-  // async sendWeatherDataToAPI(data: Forecast[]): Promise<any[]> {
-  //   const apiUrl = 'http://your-api-endpoint-ip-address'; // Replace with your actual API endpoint
-  //   return this.http.post<any[]>(apiUrl, data).toPromise();
-  // }
-  
+    const apiURL = `https://api.weather.gov/points/${location}`;
+    const gridDataUrl = await this.http.get<any>(apiURL).toPromise().then(response => response.properties.forecastGridData);
+    const forecastUrl = await this.http.get<any>(apiURL).toPromise().then(response => response.properties.forecast);
+    
+    console.log("Grid Data URL: " + gridDataUrl);
+    console.log("Forecast URL: " + forecastUrl);
+    if (!gridDataUrl || !forecastUrl) return;
+
+    const forecastData = await this.http.get<any>(forecastUrl).toPromise();
+    const gridData = await this.http.get<GridData>(gridDataUrl).toPromise();
+
+    const periods = forecastData.properties.periods.map((forecast: ForecastPeriod) => {
+      const { temperature, isDaytime, probabilityOfPrecipitation, startTime, endTime } = forecast; 
+      const precipitationProbability = probabilityOfPrecipitation?.value ?? 0;
+
+      // Find the quantitative precipitation value corresponding to the forecast period
+      const quantitativePrecip = gridData.properties.quantitativePrecipitation.values.find(precip => {
+        const precipStartTime = new Date(precip.validTime.split('/')[0]);
+        const forecastStartTime = new Date(startTime);
+        const forecastEndTime = new Date(endTime);
+        return precipStartTime >= forecastStartTime && precipStartTime < forecastEndTime;
+      })?.value ?? 0;
+
+      return {
+        temperature,
+        isDaytime,
+        precipitationProbability,
+        quantitativePrecipitation: quantitativePrecip,
+        detailedForecast: forecast.detailedForecast
+      };
+    });
+
+    console.log("Weather Data:", periods);
+    return periods;
+  }
 
   verifyRunLimiter(timestamp: number) {
     const firstTenSeconds = timestamp + 1000000;
@@ -171,5 +89,3 @@ export class WeatherApiService {
     return execute;
   }
 }
-
-
